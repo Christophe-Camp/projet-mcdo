6bf88ceab5b6db8b99e358cdfb7e14fd460575e3
PSDeSousa <paulsdesousa@hotmail.fr>
2025-03-21 08:30:42 +0100

    feat(java script): function afficherCategorie + afficherProduits

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,12 @@
 //tableau miroir du JSON
 let donnees = [];
 let tabCategories = [];
+let tabPanier = [];
 let vProduits = document.getElementById('produits'); //vignette : là où on va déposer les vignettes des produits
 let vCategories = document.getElementById('categories'); //vignette : là où on va déposer les vignettes des produits
+let panierProduits = document.getElementById('panier-produits'); //vignette : là où on va déposer les vignettes des produits
+let panierHeader = document.getElementById('panier-header'); //vignette : là où on va déposer les vignettes des produits
+
 
 fetch('mcdo.json')
   .then(function(reponse) {
@@ -13,6 +17,7 @@ fetch('mcdo.json')
     donnees = data;
     creeCategorie();
     afficherCategorie();
+    afficherProduit("sides");
   })
   .catch(function(error) {
     console.error("Erreur lors du chargement du fichier JSON :", error);
@@ -40,57 +45,49 @@ function afficherCategorie() {
     //vide les vignettes catégories
     vCategories.innerHTML = "";
 
+    tabCategories.forEach(function(categ){
 
+    let catTab = donnees[categ]
+        
     //création d'une vignette
     let div = document.createElement('div');
     //categorie[0] pour appeler l'image du premier produit de la catégorie
-    console.log(donnees.burgers[0].name);
-    let contenu = '<img src="assets/' + donnees.burgers[0].image + '" alt="burger">';
-    contenu += "<h3>burgers</h3>";
+    let contenu = '<img src="assets/' + catTab[0].image + '" alt="' + categ + '">';
+    contenu += "<h3>'" + categ + "'</h3>";
     //rempli une nouvelle div avec le contenu
     div.innerHTML = contenu;
     vCategories.appendChild(div);
 
 
-
-    //Duplique pour chaque 
-
-    div = document.createElement('div');
-    contenu = '<img src="assets/' + donnees.sides[0].image + '" alt="sides">';
-    contenu += "<h3>sides</h3>";
-    div.innerHTML = contenu;
-    vCategories.appendChild(div);
-
-    div = document.createElement('div');
-    contenu = '<img src="assets/' + donnees.drinks[0].image + '" alt="drinks">';
-    contenu += "<h3>drinks</h3>";
-    div.innerHTML = contenu;
-    vCategories.appendChild(div);
-
-
-    div = document.createElement('div');
-    contenu = '<img src="assets/' + donnees.desserts[0].image + '" alt="Desserts">';
-    contenu += "<h3>Desserts</h3>";
-    div.innerHTML = contenu;
-    vCategories.appendChild(div);
-
-
-    div = document.createElement('div');
-    contenu += '<img src="assets/' + donnees.menus[0].image + '" alt="menus">';
-    contenu += "<h3>menus</h3>";
-    div.innerHTML = contenu;
-    vCategories.appendChild(div);
-
-
-    div = document.createElement('div');
-    contenu = '<img src="assets/' + donnees.happyMeal[0].image + '" alt="happymeal">';
-    contenu += "<h3>happyMeal</h3>";
-    div.innerHTML = contenu;
-    vCategories.appendChild(div);
-
-
+})
 }
 
+function afficherProduit(prod) {
+    //vide les vignettes catégories
+    vProduits.innerHTML = "";
+
+    tabCategories.forEach(function(categ){
+
+        let catTab = donnees[categ]
+
+        //vérifie si on est sur la catégorie à filter
+        if (categ === prod){
+            for (let i = 0; i < catTab.length; i++){
+            //création d'une vignette
+            let div = document.createElement('div');
+            //categorie[i] pour appeler chaque produit de la catégorie
+            let contenu = '<img src="assets/' + catTab[i].image + '" alt="' + catTab[i].name + '">';
+            contenu += "<h3>'" + catTab[i].name + "'</h3>";
+            //rempli une nouvelle div avec le contenu
+            div.innerHTML = contenu;
+            vProduits.appendChild(div);
+            }
+        }
+    })
+}
+
+
+//pour stocker les catégories
 function creeCategorie() {
     Object.keys(donnees).forEach(function(categ){
         tabCategories.push(categ);
@@ -98,23 +95,86 @@ function creeCategorie() {
 }
 
 
+//fonction pour ajouter un produit au panier
+function ajoutProdPanier(categ, idProd){
+    
+    let catTab = donnees[categ]
+    for (let i = 0; i < catTab.length; i++){
+    if (catTab[i].id = idProd)
+    //création d'une ligne de tableau tabPanier
+    
+    
 
+
+    }
+}
+
+
+//fonction pour ajouter un produit au panier
+function ajoutMenuPanier(menu, side, drink){
+    
+}
+
+
+//fonction affichage du panier (base = table tabPanier)
+function affichPanier(){
+
+    if (tabPanier.length === 0){
+        let headRow = document.createElement('tr');
+  
+        let headContenu = "<th colspan='2'>Produit</th>";
+        headContenu += "<th>Description</th>";
+        headContenu += "<th>Prix</th>";
+        //rempli une nouvelle div avec le contenu
+        headRow.innerHTML = headContenu;
+        panierHeader.appendChild(headRow);
+    };
+    
+    for (let i = 0; i < tabPanier.length; i++){
+    //création d'une ligne de tableau
+    let row = document.createElement('tr');
+    //categorie[i] pour appeler chaque produit de la catégorie
+    let contenu = '<td><img src="assets/' + tabPanier[i].image + '" alt="' + tabPanier[i].name + '"></td>';
+    contenu += "<td>'" + tabPanier[i].name + "'</td>";
+    contenu += "<td>'" + tabPanier[i].description + "'</td>";
+    contenu += "<td>'" + tabPanier[i].price + "'</td>";
+    //rempli une nouvelle div avec le contenu
+    row.innerHTML = contenu;
+    panierProduits.appendChild(row);
+    }
+}
 
 /*
 
-function afficherCategorie() {
-    //vide les vignettes catégories
-    vCategories.innerHTML = "";
-    tabCategories.forEach(function(categ){
-        //création d'une vignette
-        let div = document.createElement('div');
-        //categorie[0] pour appeler l'image du premier produit de la catégorie
-        let contenu = '<img src="assets/images/categories/' + donnees.categ[0].image + '" alt="' + categ + '">';
-        contenu += "<h3>" + categ + "</h3>";
+//fonction pour ajouter un produit au panier
+function ajoutProdPanier(categ, prod){
+    
+    
+    
+    if (panierHeader.innerHTML === ""){
+        let headRow = document.createElement('tr');
+  
+        let headContenu = "<th colspan='2'>Produit</th>";
+        headContenu += "<th>Description</th>";
+        headContenu += "<th>Prix</th>";
         //rempli une nouvelle div avec le contenu
-        div.innerHTML = contenu;
-        vCategories.appendChild(div);
-  })
+        headRow.innerHTML = headContenu;
+        panierHeader.appendChild(headRow);
+    };
+    
+    let catTab = donnees[categ]
+    for (let i = 0; i < catTab.length; i++){
+    //création d'une ligne de tableau
+    let row = document.createElement('tr');
+    //categorie[i] pour appeler chaque produit de la catégorie
+    let contenu = '<td><img src="assets/' + catTab[i].image + '" alt="' + catTab[i].name + '"></td>';
+    contenu += "<td>'" + catTab[i].name + "'</td>";
+    contenu += "<td>'" + catTab[i].description + "'</td>";
+    contenu += "<td>'" + catTab[i].price + "'</td>";
+    //rempli une nouvelle div avec le contenu
+    row.innerHTML = contenu;
+    panierProduits.appendChild(row);
+    }
 }
 
 */
